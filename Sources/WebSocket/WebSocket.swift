d6c404e87264161dfe11e9785e4c4a530c8e6eaf
olijzenga <oebelelijzenga@gmail.com>
2019-03-21 13:30:58 +0100

    Added optional ID to WebSocket.swift

--- Sources/WebSocket/WebSocket.swift
+++ Sources/WebSocket/WebSocket.swift
@@ -71,6 +71,9 @@ public final class WebSocket: BasicWorker {
 
     /// See `onCloseCode(...)`.
     var onCloseCodeCallback: (WebSocketErrorCode) -> ()
+    
+    /// Allows for adding custom ID to a socket
+    var id: String?
 
     /// Creates a new `WebSocket` using the supplied `Channel` and `Mode`.
     /// Use `httpProtocolUpgrader(...)` to create a protocol upgrader that can create `WebSocket`s.
