c7343bc62defa947256b93deea5d7c3e75a54aea
James Hartt <jameshartt@gmail.com>
2019-10-29 10:11:14 +0000

    Add AWS websocket test

--- Tests/WebSocketKitTests/NIOWebSocketClientTests.swift
+++ Tests/WebSocketKitTests/NIOWebSocketClientTests.swift
@@ -32,18 +32,18 @@ final class NIOWebSocketClientTests: XCTestCase {
     func testBadHost() throws {
         XCTAssertThrowsError(try WebSocket.connect(host: "asdf", on: elg) { _  in }.wait())
     }
-
+    
     func testImmediateSend() throws {
         let port = Int.random(in: 8000..<9000)
-
+        
         let promise = self.elg.next().makePromise(of: String.self)
         let pongPromise = self.elg.next().makePromise(of: String.self)
-
+        
         let server = try ServerBootstrap(group: self.elg).childChannelInitializer { channel in
             let webSocket = NIOWebSocketServerUpgrader(
                 shouldUpgrade: { channel, req in
                     return channel.eventLoop.makeSucceededFuture([:])
-                },
+            },
                 upgradePipelineHandler: { channel, req in
                     return WebSocket.server(on: channel) { ws in
                         ws.send("hello")
@@ -52,18 +52,18 @@ final class NIOWebSocketClientTests: XCTestCase {
                             ws.close(promise: nil)
                         }
                     }
-                }
+            }
             )
             return channel.pipeline.configureHTTPServerPipeline(
                 withServerUpgrade: (
                     upgraders: [webSocket],
                     completionHandler: { ctx in
                         // complete
-                    }
+                }
                 )
             )
         }.bind(host: "localhost", port: port).wait()
-
+        
         WebSocket.connect(to: "ws://localhost:\(port)", on: self.elg) { ws in
             ws.send(raw: Data(), opcode: .ping)
             ws.onText { ws, string in
@@ -74,12 +74,32 @@ final class NIOWebSocketClientTests: XCTestCase {
                 pongPromise.succeed("pong")
             }
         }.cascadeFailure(to: promise)
-
+        
         try XCTAssertEqual(promise.futureResult.wait(), "goodbye")
         try XCTAssertEqual(pongPromise.futureResult.wait(), "pong")
         try server.close(mode: .all).wait()
     }
-
+    
+    func testJoinAWSWebsocket() {
+        let pongPromise = self.elg.next().makePromise(of: String.self)
+        
+        WebSocket.connect(
+            scheme: "wss",
+            host: "6cfy865zo0.execute-api.us-east-1.amazonaws.com",
+            port: 80,
+            path: "/dev", on: self.elg) { ws in
+                ws.send(raw: Data(), opcode: .ping)
+                ws.onPong { ws in
+                    pongPromise.succeed("pong")
+                }
+        }.cascadeFailure(to: pongPromise)
+        
+        pongPromise.futureResult.whenFailure { (error) in
+            debugPrint("error: \(error)")
+        }
+        try XCTAssertEqual(pongPromise.futureResult.wait(), "pong")
+    }
+    
     var elg: EventLoopGroup!
     override func setUp() {
         // needs to be at least two to avoid client / server on same EL timing issues
