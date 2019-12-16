4c9da4fe7d8186243418254031288ab5ee0202e4
James Hartt <jameshartt@gmail.com>
2019-12-16 22:09:17 +0000

    Add ping/pong callbacks to WebSocket (#45)
    * Add pong functionality
    
    * Surface close code
    
    * Optional
    
    * Headers were not being passed down to HTTPInitialRequestHandler
    
    * Add AWS websocket test
    
    * Remove extra log to trigger test run
    
    * Whitespace
    
    * Whitespace
    
    * AWS WebSocket test working
    
    * Tidy up
    
    * Add onPing so that unit tests are possible
    
    * Remove ping
    
    * Change close future to include closed code, add test for this
    
    * Headers test
    
    * Review
    
    * Remove testing manifest
    
    * Rename

--- Tests/WebSocketKitTests/NIOWebSocketClientTests.swift
+++ Tests/WebSocketKitTests/NIOWebSocketClientTests.swift
@@ -74,6 +74,123 @@ final class NIOWebSocketClientTests: XCTestCase {
         try server.close(mode: .all).wait()
     }
 
+    func testWebSocketPong() throws {
+        let port = Int.random(in: 8000..<9000)
+
+        let pongPromise = self.elg.next().makePromise(of: String.self)
+
+        let server = try ServerBootstrap(group: self.elg).childChannelInitializer { channel in
+            let webSocket = NIOWebSocketServerUpgrader(
+                shouldUpgrade: { channel, req in
+                    return channel.eventLoop.makeSucceededFuture([:])
+                },
+                upgradePipelineHandler: { channel, req in
+                    return WebSocket.server(on: channel) { ws in
+                        ws.onPing { ws in
+                            ws.close(promise: nil)
+                        }
+                    }
+                }
+            )
+            return channel.pipeline.configureHTTPServerPipeline(
+                withServerUpgrade: (
+                    upgraders: [webSocket],
+                    completionHandler: { ctx in
+                        // complete
+                    }
+                )
+            )
+        }.bind(host: "localhost", port: port).wait()
+
+        WebSocket.connect(to: "ws://localhost:\(port)", on: self.elg) { ws in
+            ws.send(raw: Data(), opcode: .ping)
+            ws.onPong { ws in
+                pongPromise.succeed("pong")
+                ws.close(promise: nil)
+            }
+        }.cascadeFailure(to: pongPromise)
+
+        try XCTAssertEqual(pongPromise.futureResult.wait(), "pong")
+        try server.close(mode: .all).wait()
+    }
+
+    func testErrorCode() throws {
+        let port = Int.random(in: 8000..<9000)
+
+        let promise = self.elg.next().makePromise(of: WebSocketErrorCode.self)
+
+        _ = try ServerBootstrap(group: self.elg).childChannelInitializer { channel in
+            let webSocket = NIOWebSocketServerUpgrader(
+                shouldUpgrade: { channel, req in
+                    return channel.eventLoop.makeSucceededFuture([:])
+                },
+                upgradePipelineHandler: { channel, req in
+                    return WebSocket.server(on: channel) { ws in
+                        ws.close(code: .normalClosure, promise: nil)
+                    }
+                }
+            )
+            return channel.pipeline.configureHTTPServerPipeline(
+                withServerUpgrade: (
+                    upgraders: [webSocket],
+                    completionHandler: { ctx in
+                        // complete
+                    }
+                )
+            )
+        }.bind(host: "localhost", port: port).wait()
+
+        WebSocket.connect(to: "ws://localhost:\(port)", on: self.elg) { ws in
+            ws.onText { ws, string in
+                ws.send("goodbye")
+            }
+            ws.onClose.whenSuccess {
+                promise.succeed(ws.closeCode!)
+                XCTAssertEqual(ws.closeCode, WebSocketErrorCode.normalClosure)
+            }
+        }.cascadeFailure(to: promise)
+
+        try XCTAssertEqual(promise.futureResult.wait(), WebSocketErrorCode.normalClosure)
+    }
+
+    func testHeadersAreSent() throws {
+        let port = Int.random(in: 8000..<9000)
+
+        let promise = self.elg.next().makePromise(of: String.self)
+
+        let server = try ServerBootstrap(group: self.elg).childChannelInitializer { channel in
+            let webSocket = NIOWebSocketServerUpgrader(
+                shouldUpgrade: { channel, req in
+                    return channel.eventLoop.makeSucceededFuture([:])
+                },
+                upgradePipelineHandler: { channel, req in
+                    promise.succeed(req.headers["Auth"].first!)
+                    return WebSocket.server(on: channel) { ws in
+                        ws.close(promise: nil)
+                    }
+                }
+            )
+            return channel.pipeline.configureHTTPServerPipeline(
+                withServerUpgrade: (
+                    upgraders: [webSocket],
+                    completionHandler: { ctx in
+                        // complete
+                    }
+                )
+            )
+        }.bind(host: "localhost", port: port).wait()
+
+        WebSocket.connect(
+            to: "ws://localhost:\(port)",
+            headers: ["Auth": "supersecretsauce"],
+            on: self.elg) { ws in
+                _ = ws.close()
+        }.cascadeFailure(to: promise)
+
+        try XCTAssertEqual(promise.futureResult.wait(), "supersecretsauce")
+        try server.close(mode: .all).wait()
+    }
+
     var elg: EventLoopGroup!
     override func setUp() {
         // needs to be at least two to avoid client / server on same EL timing issues
