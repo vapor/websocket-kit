a678f3cc0e7b5742f40484e7375c43d457237b85
tanner0101 <me@tanner.xyz>
2020-03-01 12:09:54 -0500

    rc.1

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 import PackageDescription
 
 let package = Package(
     name: "websocket-kit",
     platforms: [
-       .macOS(.v10_14)
+       .macOS(.v10_15)
     ],
     products: [
         .library(name: "WebSocketKit", targets: ["WebSocketKit"]),
@@ -15,13 +15,15 @@ let package = Package(
     ],
     targets: [
         .target(name: "WebSocketKit", dependencies: [
-            "NIO",
-            "NIOConcurrencyHelpers",
-            "NIOFoundationCompat",
-            "NIOHTTP1",
-            "NIOSSL",
-            "NIOWebSocket"
+            .product(name: "NIO", package: "swift-nio"),
+            .product(name: "NIOConcurrencyHelpers", package: "swift-nio"),
+            .product(name: "NIOFoundationCompat", package: "swift-nio"),
+            .product(name: "NIOHTTP1", package: "swift-nio"),
+            .product(name: "NIOSSL", package: "swift-nio-ssl"),
+            .product(name: "NIOWebSocket", package: "swift-nio"),
         ]),
-        .testTarget(name: "WebSocketKitTests", dependencies: ["WebSocketKit"]),
+        .testTarget(name: "WebSocketKitTests", dependencies: [
+            .target(name: "WebSocketKit"),
+        ]),
     ]
 )
