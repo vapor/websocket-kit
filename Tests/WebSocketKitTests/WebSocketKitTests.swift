31ed4e4bd5e16a7eec50985983c02062b33b7047
tanner0101 <me@tanner.xyz>
2020-08-13 16:06:28 -0400

    small close fixes

--- Tests/WebSocketKitTests/WebSocketKitTests.swift
+++ Tests/WebSocketKitTests/WebSocketKitTests.swift
@@ -6,15 +6,17 @@ import NIOWebSocket
 
 final class WebSocketKitTests: XCTestCase {
     func testWebSocketEcho() throws {
-        let promise = elg.next().makePromise(of: String.self)
-        WebSocket.connect(to: "ws://echo.websocket.org", on: elg) { ws in
-            ws.send("hello")
-            ws.onText { ws, string in
-                promise.succeed(string)
-                ws.close(promise: nil)
-            }
-        }.cascadeFailure(to: promise)
-        try XCTAssertEqual(promise.futureResult.wait(), "hello")
+            let promise = elg.next().makePromise(of: String.self)
+            let closePromise = elg.next().makePromise(of: Void.self)
+            WebSocket.connect(to: "ws://echo.websocket.org", on: elg) { ws in
+                ws.send("hello")
+                ws.onText { ws, string in
+                    promise.succeed(string)
+                    ws.close(promise: closePromise)
+                }
+            }.cascadeFailure(to: promise)
+            try XCTAssertEqual(promise.futureResult.wait(), "hello")
+            XCTAssertNoThrow(try closePromise.futureResult.wait())
     }
     
     func testWebSocketWithTLSEcho() throws {
@@ -33,34 +35,69 @@ final class WebSocketKitTests: XCTestCase {
         XCTAssertThrowsError(try WebSocket.connect(host: "asdf", on: elg) { _  in }.wait())
     }
 
+    func testServerClose() throws {
+        let port = Int.random(in: 8000..<9000)
+
+        let sendPromise = self.elg.next().makePromise(of: Void.self)
+        let serverClose = self.elg.next().makePromise(of: Void.self)
+        let clientClose = self.elg.next().makePromise(of: Void.self)
+        let server = try ServerBootstrap.webSocket(on: self.elg) { req, ws in
+            ws.onText { ws, text in
+                if text == "close" {
+                    ws.close(promise: serverClose)
+                }
+            }
+        }.bind(host: "localhost", port: port).wait()
+
+        WebSocket.connect(to: "ws://localhost:\(port)", on: self.elg) { ws in
+            ws.send("close", promise: sendPromise)
+            ws.onClose.cascade(to: clientClose)
+        }.cascadeFailure(to: sendPromise)
+
+        XCTAssertNoThrow(try sendPromise.futureResult.wait())
+        XCTAssertNoThrow(try serverClose.futureResult.wait())
+        XCTAssertNoThrow(try clientClose.futureResult.wait())
+        try server.close(mode: .all).wait()
+    }
+
+    func testClientClose() throws {
+        let port = Int.random(in: 8000..<9000)
+
+        let sendPromise = self.elg.next().makePromise(of: Void.self)
+        let serverClose = self.elg.next().makePromise(of: Void.self)
+        let clientClose = self.elg.next().makePromise(of: Void.self)
+        let server = try ServerBootstrap.webSocket(on: self.elg) { req, ws in
+            ws.onText { ws, text in
+                ws.send(text)
+            }
+            ws.onClose.cascade(to: serverClose)
+        }.bind(host: "localhost", port: port).wait()
+
+        WebSocket.connect(to: "ws://localhost:\(port)", on: self.elg) { ws in
+            ws.send("close", promise: sendPromise)
+            ws.onText { ws, text in
+                if text == "close" {
+                    ws.close(promise: clientClose)
+                }
+            }
+        }.cascadeFailure(to: sendPromise)
+
+        XCTAssertNoThrow(try sendPromise.futureResult.wait())
+        XCTAssertNoThrow(try serverClose.futureResult.wait())
+        XCTAssertNoThrow(try clientClose.futureResult.wait())
+        try server.close(mode: .all).wait()
+    }
+
     func testImmediateSend() throws {
         let port = Int.random(in: 8000..<9000)
 
         let promise = self.elg.next().makePromise(of: String.self)
-
-        let server = try ServerBootstrap(group: self.elg).childChannelInitializer { channel in
-            let webSocket = NIOWebSocketServerUpgrader(
-                shouldUpgrade: { channel, req in
-                    return channel.eventLoop.makeSucceededFuture([:])
-                },
-                upgradePipelineHandler: { channel, req in
-                    return WebSocket.server(on: channel) { ws in
-                        ws.send("hello")
-                        ws.onText { ws, string in
-                            promise.succeed(string)
-                            ws.close(promise: nil)
-                        }
-                    }
-                }
-            )
-            return channel.pipeline.configureHTTPServerPipeline(
-                withServerUpgrade: (
-                    upgraders: [webSocket],
-                    completionHandler: { ctx in
-                        // complete
-                    }
-                )
-            )
+        let server = try ServerBootstrap.webSocket(on: self.elg) { req, ws in
+            ws.send("hello")
+            ws.onText { ws, string in
+                promise.succeed(string)
+                ws.close(promise: nil)
+            }
         }.bind(host: "localhost", port: port).wait()
 
         WebSocket.connect(to: "ws://localhost:\(port)", on: self.elg) { ws in
@@ -78,28 +115,10 @@ final class WebSocketKitTests: XCTestCase {
         let port = Int.random(in: 8000..<9000)
 
         let pongPromise = self.elg.next().makePromise(of: String.self)
-
-        let server = try ServerBootstrap(group: self.elg).childChannelInitializer { channel in
-            let webSocket = NIOWebSocketServerUpgrader(
-                shouldUpgrade: { channel, req in
-                    return channel.eventLoop.makeSucceededFuture([:])
-                },
-                upgradePipelineHandler: { channel, req in
-                    return WebSocket.server(on: channel) { ws in
-                        ws.onPing { ws in
-                            ws.close(promise: nil)
-                        }
-                    }
-                }
-            )
-            return channel.pipeline.configureHTTPServerPipeline(
-                withServerUpgrade: (
-                    upgraders: [webSocket],
-                    completionHandler: { ctx in
-                        // complete
-                    }
-                )
-            )
+        let server = try ServerBootstrap.webSocket(on: self.elg) { req, ws in
+            ws.onPing { ws in
+                ws.close(promise: nil)
+            }
         }.bind(host: "localhost", port: port).wait()
 
         WebSocket.connect(to: "ws://localhost:\(port)", on: self.elg) { ws in
@@ -119,25 +138,8 @@ final class WebSocketKitTests: XCTestCase {
 
         let promise = self.elg.next().makePromise(of: WebSocketErrorCode.self)
 
-        _ = try ServerBootstrap(group: self.elg).childChannelInitializer { channel in
-            let webSocket = NIOWebSocketServerUpgrader(
-                shouldUpgrade: { channel, req in
-                    return channel.eventLoop.makeSucceededFuture([:])
-                },
-                upgradePipelineHandler: { channel, req in
-                    return WebSocket.server(on: channel) { ws in
-                        ws.close(code: .normalClosure, promise: nil)
-                    }
-                }
-            )
-            return channel.pipeline.configureHTTPServerPipeline(
-                withServerUpgrade: (
-                    upgraders: [webSocket],
-                    completionHandler: { ctx in
-                        // complete
-                    }
-                )
-            )
+        let server = try ServerBootstrap.webSocket(on: self.elg) { req, ws in
+            ws.close(code: .normalClosure, promise: nil)
         }.bind(host: "localhost", port: port).wait()
 
         WebSocket.connect(to: "ws://localhost:\(port)", on: self.elg) { ws in
@@ -151,6 +153,7 @@ final class WebSocketKitTests: XCTestCase {
         }.cascadeFailure(to: promise)
 
         try XCTAssertEqual(promise.futureResult.wait(), WebSocketErrorCode.normalClosure)
+        try server.close(mode: .all).wait()
     }
 
     func testHeadersAreSent() throws {
@@ -158,26 +161,9 @@ final class WebSocketKitTests: XCTestCase {
 
         let promise = self.elg.next().makePromise(of: String.self)
 
-        let server = try ServerBootstrap(group: self.elg).childChannelInitializer { channel in
-            let webSocket = NIOWebSocketServerUpgrader(
-                shouldUpgrade: { channel, req in
-                    return channel.eventLoop.makeSucceededFuture([:])
-                },
-                upgradePipelineHandler: { channel, req in
-                    promise.succeed(req.headers["Auth"].first!)
-                    return WebSocket.server(on: channel) { ws in
-                        ws.close(promise: nil)
-                    }
-                }
-            )
-            return channel.pipeline.configureHTTPServerPipeline(
-                withServerUpgrade: (
-                    upgraders: [webSocket],
-                    completionHandler: { ctx in
-                        // complete
-                    }
-                )
-            )
+        let server = try ServerBootstrap.webSocket(on: self.elg) { req, ws in
+            promise.succeed(req.headers.first(name: "Auth")!)
+            ws.close(promise: nil)
         }.bind(host: "localhost", port: port).wait()
 
         WebSocket.connect(
@@ -191,6 +177,42 @@ final class WebSocketKitTests: XCTestCase {
         try server.close(mode: .all).wait()
     }
 
+    func testLocally() throws {
+        // swap to test websocket server against local client
+        try XCTSkipIf(true)
+
+        let port = Int(1337)
+        let shutdownPromise = self.elg.next().makePromise(of: Void.self)
+
+        let server = try! ServerBootstrap.webSocket(on: self.elg) { req, ws in
+            ws.send("welcome!")
+
+            ws.onClose.whenComplete {
+                print("ws.onClose done: \($0)")
+            }
+
+            ws.onText { ws, text in
+                switch text {
+                case "shutdown":
+                    shutdownPromise.succeed(())
+                case "close":
+                    ws.close().whenComplete {
+                        print("ws.close() done \($0)")
+                    }
+                default:
+                    ws.send(text.reversed())
+                }
+            }
+        }.bind(host: "localhost", port: port).wait()
+        print("Serving at ws://localhost:\(port)")
+
+        print("Waiting for server shutdown...")
+        try shutdownPromise.futureResult.wait()
+
+        print("Waiting for server close...")
+        try server.close(mode: .all).wait()
+    }
+
     var elg: EventLoopGroup!
     override func setUp() {
         // needs to be at least two to avoid client / server on same EL timing issues
@@ -198,5 +220,33 @@ final class WebSocketKitTests: XCTestCase {
     }
     override func tearDown() {
         try! self.elg.syncShutdownGracefully()
+    }
+}
+
+extension ServerBootstrap {
+    static func webSocket(
+        on eventLoopGroup: EventLoopGroup,
+        onUpgrade: @escaping (HTTPRequestHead, WebSocket) -> ()
+    ) -> ServerBootstrap {
+        ServerBootstrap(group: eventLoopGroup).childChannelInitializer { channel in
+            let webSocket = NIOWebSocketServerUpgrader(
+                shouldUpgrade: { channel, req in
+                    return channel.eventLoop.makeSucceededFuture([:])
+                },
+                upgradePipelineHandler: { channel, req in
+                    return WebSocket.server(on: channel) { ws in
+                        onUpgrade(req, ws)
+                    }
+                }
+            )
+            return channel.pipeline.configureHTTPServerPipeline(
+                withServerUpgrade: (
+                    upgraders: [webSocket],
+                    completionHandler: { ctx in
+                        // complete
+                    }
+                )
+            )
+        }
     }
 }
