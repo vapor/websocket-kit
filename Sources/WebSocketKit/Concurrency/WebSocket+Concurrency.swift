3c8a39a3ae90b85b0fd96418869b48532a754a86
Mads Odgaard <mads@madsodgaard.com>
2021-10-27 14:54:23 +0200

    Use NIOCore and updated availability checks

--- Sources/WebSocketKit/Concurrency/WebSocket+Concurrency.swift
+++ Sources/WebSocketKit/Concurrency/WebSocket+Concurrency.swift
@@ -1,10 +1,9 @@
+#if compiler(>=5.5) && canImport(_Concurrency)
+import NIOCore
 import NIOWebSocket
 import Foundation
 
-#if compiler(>=5.5)
-import _NIOConcurrency
-
-@available(macOS 9999, iOS 9999, watchOS 9999, tvOS 9999, *)
+@available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
 extension WebSocket {
     public func send<S>(_ text: S) async throws
         where S: Collection, S.Element == Character
@@ -15,11 +14,15 @@ extension WebSocket {
     }
 
     public func send(_ binary: [UInt8]) async throws {
-        try await send(raw: binary, opcode: .binary, fin: true)
+        let promise = eventLoop.makePromise(of: Void.self)
+        send(binary, promise: promise)
+        return try await promise.futureResult.get()
     }
 
     public func sendPing() async throws {
-        try await send(raw: Data(), opcode: .ping, fin: true)
+        let promise = eventLoop.makePromise(of: Void.self)
+        sendPing(promise: promise)
+        return try await promise.futureResult.get()
     }
 
     public func send<Data>(
@@ -69,5 +72,74 @@ extension WebSocket {
             }
         }
     }
+
+    public static func connect(
+        to url: String,
+        headers: HTTPHeaders = [:],
+        configuration: WebSocketClient.Configuration = .init(),
+        on eventLoopGroup: EventLoopGroup,
+        onUpgrade: @escaping (WebSocket) async -> ()
+    ) async throws {
+        return try await self.connect(
+            to: url,
+            headers: headers,
+            configuration: configuration,
+            on: eventLoopGroup,
+            onUpgrade: { ws in
+                Task {
+                    await onUpgrade(ws)
+                }
+            }
+        ).get()
+    }
+
+    public static func connect(
+        to url: URL,
+        headers: HTTPHeaders = [:],
+        configuration: WebSocketClient.Configuration = .init(),
+        on eventLoopGroup: EventLoopGroup,
+        onUpgrade: @escaping (WebSocket) async -> ()
+    ) async throws {
+        return try await self.connect(
+            to: url,
+            headers: headers,
+            configuration: configuration,
+            on: eventLoopGroup,
+            onUpgrade: { ws in
+                Task {
+                    await onUpgrade(ws)
+                }
+            }
+        ).get()
+    }
+
+    public static func connect(
+        scheme: String = "ws",
+        host: String,
+        port: Int = 80,
+        path: String = "/",
+        query: String? = nil,
+        headers: HTTPHeaders = [:],
+        configuration: WebSocketClient.Configuration = .init(),
+        on eventLoopGroup: EventLoopGroup,
+        onUpgrade: @escaping (WebSocket) async -> ()
+    ) async throws {
+        return try await self.connect(
+            scheme: scheme,
+            host: host,
+            port: port,
+            path: path,
+            query: query,
+            headers: headers,
+            configuration: configuration,
+            on: eventLoopGroup,
+            onUpgrade: { ws in
+                Task {
+                    await onUpgrade(ws)
+                }
+            }
+        ).get()
+    }
 }
+
 #endif
---
+++ Tests/WebSocketKitTests/AsyncWebSocketKitTests.swift
@@ -0,0 +1,57 @@
+#if compiler(>=5.5) && canImport(_Concurrency)
+#if !os(Linux)
+import XCTest
+import NIO
+import NIOHTTP1
+import NIOWebSocket
+@testable import WebSocketKit
+
+@available(macOS 12, iOS 15, watchOS 8, tvOS 15, *)
+final class AsyncWebSocketKitTests: XCTestCase {
+    func testWebSocketEcho() async throws {
+        let server = try ServerBootstrap.webSocket(on: self.elg) { req, ws in
+            ws.onText { ws, text in
+                ws.send(text)
+            }
+        }.bind(host: "localhost", port: 0).wait()
+
+        guard let port = server.localAddress?.port else {
+            XCTFail("couldn't get port from \(server.localAddress.debugDescription)")
+            return
+        }
+
+        let promise = elg.next().makePromise(of: String.self)
+
+        try await WebSocket.connect(to: "ws://localhost:\(port)", on: elg) { ws in
+            do {
+                try await ws.send("hello")
+                ws.onText { ws, string in
+                    promise.succeed(string)
+                    do {
+                        try await ws.close()
+                    } catch {
+                        XCTFail("Failed to close websocket, error: \(error)")
+                    }
+                }
+            } catch {
+                promise.fail(error)
+            }
+        }
+
+        let result = try await promise.futureResult.get()
+        XCTAssertEqual(result, "hello")
+        try await server.close(mode: .all)
+    }
+
+    var elg: EventLoopGroup!
+    override func setUp() {
+        // needs to be at least two to avoid client / server on same EL timing issues
+        self.elg = MultiThreadedEventLoopGroup(numberOfThreads: 2)
+    }
+    override func tearDown() {
+        try! self.elg.syncShutdownGracefully()
+    }
+}
+
+#endif
+#endif
