5192d09caabc2f347b0ba1079c9194484494da37
Mads Odgaard <mads@madsodgaard.com>
2021-10-27 14:55:52 +0200

    Remove _NIOConcurrency as dependency

--- Package.swift
+++ Package.swift
@@ -22,7 +22,6 @@ let package = Package(
             .product(name: "NIOHTTP1", package: "swift-nio"),
             .product(name: "NIOSSL", package: "swift-nio-ssl"),
             .product(name: "NIOWebSocket", package: "swift-nio"),
-            .product(name: "_NIOConcurrency", package: "swift-nio"),
         ]),
         .testTarget(name: "WebSocketKitTests", dependencies: [
             .target(name: "WebSocketKit"),
