04cf7d754f9bc1dd2fff3f2faa6c50946c968005
Pop Flamingo <8317047+PopFlamingo@users.noreply.github.com>
2022-04-27 14:09:19 +0200

    Update Package.swift
    Co-authored-by: Gwynne Raskind <gwynne@darkrainfall.org>

--- Package.swift
+++ Package.swift
@@ -4,7 +4,10 @@ import PackageDescription
 let package = Package(
     name: "websocket-kit",
     platforms: [
-        .macOS(.v10_15), .watchOS(.v6)
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13),
     ],
     products: [
         .library(name: "WebSocketKit", targets: ["WebSocketKit"]),
