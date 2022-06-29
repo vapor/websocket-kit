d8230eae70650652661f360f322db0cc22955b2f
Maxim Kolesnik <makleso6@gmail.com>
2022-06-29 17:34:08 +0300

    add iOS 11 platform (#118)
    Co-authored-by: Maxim Kolesnik <kolesnik@yandex-team.ru>

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "websocket-kit",
     platforms: [
-       .macOS(.v10_15)
+       .macOS(.v10_15),
+       .iOS(.v11)
     ],
     products: [
         .library(name: "WebSocketKit", targets: ["WebSocketKit"]),
